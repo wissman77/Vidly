9bc51e2609fe65534a4a20dd58224c61543f35b1
wissman77 <43609409+wissman77@users.noreply.github.com>
2020-11-17 20:48:59 +0200

    Finishing Exercise

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers;
+
+        public CustomersController()
+        {
+            customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith"},
+                new Customer { Id = 2, Name = "Mary Williams"}
+            };
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Detail(int Id)
+        {
+            return View(customers.FirstOrDefault(c => c.Id == Id));
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,8 +14,40 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel();
+            viewModel.Movie = movie;
+            viewModel.Customers = customers;
+            
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>()
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+
+            return View(movies);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
