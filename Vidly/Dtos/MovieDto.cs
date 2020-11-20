90d47469787ede9a93786185f1e94b901be9c486
wissman77 <43609409+wissman77@users.noreply.github.com>
2020-11-20 13:29:49 +0200

    Adding Customers and Movies API

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public short GenreId { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
